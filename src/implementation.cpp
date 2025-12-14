42ef39a81317c4edee1078c20c77ca608678bd7b
Cauã Anacleto <anacleto.for.work@gmail.com>
2025-12-14 15:53:33 -0300

    Refactor: Removed pragma once on source file, added comments

--- src/implementation.cpp
+++ src/implementation.cpp
@@ -1,7 +1,12 @@
-#pragma once
+/** 
+ *  Translation Unit for Vendored Libraries
+ * 
+*/
 
+// Source: https://github.com/nothings/stb/blob/master/stb_image.h
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
+// Source: https://github.com/tinyobjloader/tinyobjloader/blob/release/tiny_obj_loader.h
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
