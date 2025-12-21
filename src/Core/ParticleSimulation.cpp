e681fcfcc4718fe1adae6978550d7bd012c17f84
Cauã Anacleto <anacleto.for.work@gmail.com>
2025-12-21 17:55:07 -0300

    Refactor: Correct usage flags for texture creation stagging buffer

--- src/Core/ParticleSimulation.cpp
+++ src/Core/ParticleSimulation.cpp
@@ -848,7 +848,7 @@ class ParticleSimulation {
         GpuBuffer stagingBuffer = GpuBuffer(
             *m_deviceCtx,
             imageSize,
-            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+            VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
             m_deviceCtx->m_transferQueueCtx
         );
